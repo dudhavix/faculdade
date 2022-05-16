b8234a2b6f4197f497b394c27c11622c06db2c3e
dudhavix <53200316+dudhavix@users.noreply.github.com>
2022-05-16 17:39:25 -0300

    versão 1

---
+++ src/components/loading.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="tela-loading" class="d-flex justify-content-center align-items-center">
+        <div id="spinner" class="spinner-border text-light" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "loading"
+}
+</script>
+
+<style scoped>
+#tela-loading{
+    position: fixed;
+    z-index: 9;
+    height: 100vh;
+    width: 100vw;
+    background-color: #7b809a;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        token: null
+    },
+    mutations: {
+        setToken(state, payload){
+            state.token = payload;
+        },
+    },
+    actions: {
+    },
+    modules: {
+    }
 })
