9f2d94e875252e2b2e2f0cff490991bc6f792abc
dudhavix <53200316+dudhavix@users.noreply.github.com>
2022-06-18 13:56:19 -0300

    ajustes

--- teste.js
+++
@@ -1,6 +0,0 @@
-const logger = require("./config/helper-log");
-
-logger.warning("comunidade", "teste de mensagem");
-logger.error("comunidade", "teste de mensagem");
-logger.success("comunidade", "teste de mensagem");
-logger.debug("comunidade", "teste de mensagem");
