557fbb968a81a6c57f451b66574bce106a25bb78
dudhavix <53200316+dudhavix@users.noreply.github.com>
2022-06-19 00:49:10 -0300

    ajustes

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        hostServidor: "http://localhost:5000",
+        hostServidor: "https://walking-challenger-backend.herokuapp.com",
         
         token: null,
         usuario: null,
